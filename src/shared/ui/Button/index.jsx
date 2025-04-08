9ca2b743539f6adda946f75bd77db786ae4a4db5
do <ed0619@naver.com>
2025-04-08 17:27:54 +0900

    해더 css,jsx

--- src/shared/ui/Button/index.jsx
+++ src/shared/ui/Button/index.jsx
@@ -1,10 +1,15 @@
+
+
 import "./Button.css"
 
-export default function Button({value,onClick,disabled,size}){
+
+
+export default function Button({value,onClick,disabled,size,Children}){
 
   return(<>
       <div className="btn-container">
         <button className={`btn-${size}`} onClick={onClick} disabled={disabled}>{value}</button>
+        {Children}
       </div>
     </>)
 }
