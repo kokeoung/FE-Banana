fdc95ece45ecf1bebd202465979080cc0fbde384
kokeoung <ko908599@gmail.com>
2025-04-08 17:24:30 +0900

    메인화면 PostCard

---
+++ src/shared/ui/UserProfile/index.jsx
@@ -0,0 +1,15 @@
+import './UserProfile.css';
+
+export default function UserProfile({profileImage,nickname}){
+  return(<>
+    <div className="profile-container">
+      <div className="profile-img">
+        <img src={profileImage}/>
+      </div>
+      <div className="proflie-nick">
+        <span id="by">by</span>&nbsp;
+        {nickname} 
+      </div>
+    </div>
+  </>)
+}
