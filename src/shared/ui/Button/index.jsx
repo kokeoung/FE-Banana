bc52985c940b21c88a9a7b4723d49329b36a292e
조준희 <soyre1928@gmail.com>
2025-04-08 18:22:57 +0900

    button css 수정

--- src/shared/ui/Button/index.jsx
+++ src/shared/ui/Button/index.jsx
@@ -1,10 +1,6 @@
-
-
 import "./Button.css"
 
-export default function Button({value,onClick,disabled,size,Children}){
-
-
+export default function Button({value,onClick,disabled,size="m",Children}){
   return(<>
       <div className="btn-container">
         <button className={`btn-${size}`} onClick={onClick} disabled={disabled}>{value}</button>
