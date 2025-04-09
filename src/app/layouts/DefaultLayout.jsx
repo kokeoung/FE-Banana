704adfead942b9c1248f8c0e27a44dfe5d6b5bd3
do <ed0619@naver.com>
2025-04-09 17:00:09 +0900

    글 읽기 페이지

--- src/app/layouts/DefaultLayout.jsx
+++ src/app/layouts/DefaultLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Header from "../../widgets/Header";
 
+
 const DefaultLayout=()=>{
   
   return (<>
--- src/pages/ReadPage/index.jsx
+++ src/pages/ReadPage/index.jsx
@@ -1,5 +1,52 @@
+import Button from '../../shared/ui/Button';
+import './ReadPage.css';
 export default function ReadPage(){
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  
+  const formatted = `${year}.${month}.${day}`;
+
   return(<>
-    <h1>읽기</h1>
+  <main>
+  <section className='post-detail'> 
+    {/* ////////////////////////////////////// */}
+    <div className='post-detail-header'>
+      <div className='title'>
+        <title>게시물 읽기</title>
+      </div>
+      <div className='post-medle-title'>
+        <h2> </h2>
+      </div>
+      <div className='post-thumbnail'>
+        <img />
+      </div>
+    </div>
+    {/* ////////////////////////////////////// */}
+    <div className='post-detail-content'>
+        <p>게시물 제목</p>
+    </div>
+    <div className='post-information'>
+      <div className='username'>
+        <a>유저 이름</a>
+
+      </div>
+      <div className='separator'>
+        <span>{formatted}</span>
+      </div>
+      <div className='flow-btn'> 
+      <Button size={"s"} value="팔로잉"/>
+      </div>
+      <div className='heart-btn'>
+      <Button size={"s"}/>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+    {/* ////////////////////////////////////// */}
+  </section>
+    </main>
   </>)
 }
