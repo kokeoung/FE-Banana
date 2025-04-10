9ed6b2502542ba156d0259cdf65da740e3916ed4
do <ed0619@naver.com>
2025-04-10 14:19:32 +0900

    읽기 페이지 50%

--- src/pages/ReadPage/index.jsx
+++ src/pages/ReadPage/index.jsx
@@ -1,5 +1,10 @@
+import { FaHeart, FaShareAlt } from 'react-icons/fa';
 import Button from '../../shared/ui/Button';
+import  readImg from './img/img1.jpg';
 import './ReadPage.css';
+
+
+
 export default function ReadPage(){
   const today = new Date();
   const year = today.getFullYear();
@@ -7,8 +12,14 @@ export default function ReadPage(){
   const day = String(today.getDate()).padStart(2, '0');
   
   const formatted = `${year}.${month}.${day}`;
+  
+    
+
+  
+
 
   return(<>
+  <hr />
   <main>
   <section className='post-detail'> 
     {/* ////////////////////////////////////// */}
@@ -28,22 +39,34 @@ export default function ReadPage(){
         <p>게시물 제목</p>
     </div>
     <div className='post-information'>
-      <div className='username'>
-        <a>유저 이름</a>
-
-      </div>
-      <div className='separator'>
-        <span>{formatted}</span>
+      
+      <div className='user-info'>
+          <div className='username'>
+            <span>유저 이름</span>
+          </div>
+        <span>&nbsp;&nbsp;{formatted}</span>
       </div>
       <div className='flow-btn'> 
-      <Button size={"s"} value="팔로잉"/>
+      <Button size={"m"} value="팔로잉" />
       </div>
       <div className='heart-btn'>
-      <Button size={"s"}/>
+      <Button size={"m"} className='btn-h2' />
+      <section className='cord-area'>
+      <div className='image-wrapper'>
+        <img src={readImg}  className='read-img1'/>
       </div>
-      <div>
-        
-      </div>
+        <button className='btn-icon-panel'>
+            <div className='btn-icon-Wrapper'>
+              <FaHeart className='btn-icon' />
+            </div>
+            <div className='btn-icon-count'>30</div>
+            <div className='btn-icon-Wrapper'>
+              <FaShareAlt className='btn-icon' />
+            </div>
+        </button>
+      
+      </section>
+    </div>  
     </div>
     {/* ////////////////////////////////////// */}
   </section>
