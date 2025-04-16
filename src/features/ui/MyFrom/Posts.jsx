dd211f4b237fcf46d5493e5251a7fbc8d0f866b4
조준희 <soyre1928@gmail.com>
2025-04-16 18:11:59 +0900

    my페이지 이동시 유저 id로 이동, 유저 id를 통해 백엔드로부터 데이터 가져옴

--- src/features/ui/MyFrom/Posts.jsx
+++ src/features/ui/MyFrom/Posts.jsx
@@ -2,6 +2,7 @@ import { IoSearchOutline } from "react-icons/io5"
 import "./MyFrom.css"
 import { useState } from "react"
 import MyPostCard from "../../../shared/ui/MyPostCard"
+import none from "../../../app/assets/notcontent.svg"
 
 const dummy = [
   {
@@ -9,7 +10,7 @@ const dummy = [
     title: '테스트1',
     img: 'https://mblogthumb-phinf.pstatic.net/20120615_30/snaps1_1339721440666NgJXG_JPEG/%BA%B0%BB%E7%C1%F8%C0%DF%C2%EF%B4%C2%B9%FD%B9%E3%BE%DF%B0%E6%BB%E7%C1%F8%C0%DF%C2%EF%B4%C2%B9%FD.jpg?type=w420',
     createdAt: '2025년 04월 06일',
-    content:"동해물과 백두산이 마르고 닳도록",
+    content:"길어져도 문제 없는지 테스트 길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트길어져도 문제 없는지 테스트",
     like: 12,
   },
   {
@@ -31,7 +32,6 @@ const dummy = [
 ]
 
 
-
 export default function Posts(){
 
   const [color,setColor] = useState("")
@@ -40,7 +40,6 @@ export default function Posts(){
   const filteredData = dummy.filter(post =>
     post.title.toLowerCase().includes(search.toLowerCase())
   );
-
   return(<>
     <div className="postspage-container">
       <div className="postspage-header">
@@ -50,16 +49,21 @@ export default function Posts(){
             onFocus={() => setColor("postspage-focus")} onBlur={() => setColor("")} onChange={e => setSearch(e.target.value)}/>
         </div>
       </div>
-      <div className="postspage-main">
-        {filteredData.map(post=>(
-          <MyPostCard key={post.id} 
-            imageUrl={post.img} 
-            title={post.title} 
-            content={post.content} 
-            createdAt={post.createdAt} 
-            liked={post.like} />
-        ))}
-      </div>
+      {dummy?
+      (<div className="postspage-worng">
+        <img src={none} alt="" />
+      </div>):
+      (<div className="postspage-main">
+      {filteredData.map(post=>(
+        <MyPostCard key={post.id} 
+          imageUrl={post.img} 
+          title={post.title} 
+          content={post.content} 
+          createdAt={post.createdAt} 
+          liked={post.like} />
+      ))}
+      </div>)
+      }
     </div>
   </>)
 }
--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,23 +1,60 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 import "./MyPage.css"
 import MyUserProfile from "../../shared/ui/MyUserProfile";
 import img from "../../app/assets/cloud.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function MyPage(){
-  const user = JSON.parse(localStorage.getItem('user'));
-  
-  const [active,setActive] = useState({
-    myli1:"",
-    myli2:"",
-    myli3:""
-  })
+  const { userId } = useParams();
+  const [active,setActive] = useState({myli1:"",myli2:"",myli3:""});
+  const [user,setUser] = useState([""]);
 
+  useEffect(() => {
+      
+    async function fetchUser(){
+      try {
+        // setIsLoading(true);
+        const url = `http://localhost:8080/api/my/${userId}`;
+        const sendData = {
+            userId: userId,
+        }
+        const init = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(sendData)
+        }
+        const response = await fetch(url, init);
+        console.log(userId)
+        const data = await response.json();
+        setUser(data);
+      } catch (err) {
+        // setError(err);
+      } finally {
+        // setIsLoading(false);
+      }
+      };
+      fetchUser();
+  
+      // 헤더 정보 업데이트
+      // setPageInfo({
+      //   title: 'B',
+      //   author: post.author.username,
+      //   isHome: false
+      // });
+      
+      // 컴포넌트 언마운트 시 헤더 정보 초기화
+      // return () => {
+      //   setPageInfo({
+      //     title: 'Banana',
+      //     author: null,
+      //     isHome: true
+      //   });
+    // };
+  }, []);
   function handleClick(e){
     const {id} = e.currentTarget;
     setActive({myli1:"",myli2:"",myli3:""})
     setActive(prev => ({...prev,[id]:"active"}))
-    console.log(user);
   }
 
   return(<>
@@ -30,13 +67,13 @@ export default function MyPage(){
       <div className="my-main">
         <ul>
           <li className={`my-nav-${active.myli1}`} onClick={handleClick} id="myli1">
-            <Link to={'/my/posts'}>글</Link>
+            <Link to={`/my/${userId}/posts`}>글</Link>
           </li>
           <li className={`my-nav-${active.myli2}`} onClick={handleClick} id="myli2">
-            <Link to={'/my/series'}>시리즈</Link>
+            <Link to={`/my/${userId}/series`}>시리즈</Link>
           </li>
           <li className={`my-nav-${active.myli3}`} onClick={handleClick} id="myli3">
-            <Link to={'/my/about'}>소개</Link>
+            <Link to={`/my/${userId}/about`}>소개</Link>
           </li>
         </ul>
       </div>
