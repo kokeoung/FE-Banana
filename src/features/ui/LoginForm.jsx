36040a21319b86c67449a3287a230f68b7c6f365
조준희 <soyre1928@gmail.com>
2025-04-17 15:34:24 +0900

    버전업 하기전 자료

--- src/features/ui/LoginForm.jsx
+++ src/features/ui/LoginForm.jsx
@@ -3,7 +3,7 @@ import Input from "@shared/ui/Input"
 import "./AuthForm.css"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import Modal from "../../shared/ui/modal"
+import Modal from "../../shared/ui/Modal"
 
 export default function LoginForm(){
 
