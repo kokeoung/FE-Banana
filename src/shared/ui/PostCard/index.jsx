8cf9f39ef42fcecc42b89192777764ce4aa03c41
kokeoung <ko908599@gmail.com>
2025-04-23 01:25:44 +0900

    메인페이지 포스트카드데이터받아오기, 포스트카드클릭 시 게시글상세페이지 이동, css수정

--- src/shared/ui/PostCard/index.jsx
+++ src/shared/ui/PostCard/index.jsx
@@ -1,23 +1,34 @@
 import './PostCard.css';
+import { Link } from 'react-router-dom';
 
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // 월은 0부터 시작하므로 +1
+  const day = date.getDate();
 
-export default function PostCard({title,imageUrl,createdAt,likes,children}){
+  return `${year}년 ${month}월 ${day}일`;
+};
+
+export default function PostCard({postId, postTitle,postContent,imageUrl,createDate,likeCount,children}){
 
   return(<>
+  <Link href={`/posts/${postId}`} className="postcard-link"></Link>
     <div className="postcard-card">
       <div className="postcard-header">
         <img src={imageUrl} className="postcard-img"/>
       </div>
       <div className="postcard-main">
-        <div className="postcard-title">{title}</div>
-        <div className="postcard-date">{createdAt}</div>
+        <div className="postcard-title">{postTitle}</div>
+        <div className="postcard-content">{postContent}</div>
+        <div className="postcard-date">{createDate}</div>
       </div>
       <div className="postcard-footer">
 
         <div>{children}</div>
         <div className="postcard-like">
           <div>🖤</div>
-          <div className="postcard-likes">{likes}</div>
+          <div className="postcard-likes">{likeCount}</div>
 
         </div>
       </div>
--- src/shared/ui/UserProfile/index.jsx
+++ src/shared/ui/UserProfile/index.jsx
@@ -1,14 +1,14 @@
 import './UserProfile.css';
 
-export default function UserProfile({profileImage,nickname}){
+export default function UserProfile({userProfileImage,userNick}){
   return(<>
     <div className="profile-container">
       <div className="profile-img">
-        <img src={profileImage}/>
+        <img src={userProfileImage}/>
       </div>
       <div className="proflie-nick">
         <span id="by">by</span>&nbsp;
-        {nickname} 
+        {userNick} 
       </div>
     </div>
   </>)
