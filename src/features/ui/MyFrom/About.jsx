f0d88a2a032d46ff73f9e12ac7b5fb1b9082e8c8
조준희 <soyre1928@gmail.com>
2025-04-23 09:10:46 +0900

    커밋할 내용 있는지 확인

--- src/features/ui/MyFrom/About.jsx
+++ src/features/ui/MyFrom/About.jsx
@@ -1,5 +1,10 @@
+import none from "../../../app/assets/notcontent2.svg"
+
 export default function About(){
   return(<>
   <h1>소개</h1>
+  <div className="postspage-worng">
+    <img src={none} alt="" />
+  </div>
   </>)
 }
